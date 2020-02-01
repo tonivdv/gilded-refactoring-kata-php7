dd06b93b19c771d115c80bb98ab763b553d602cc
tonivdv <toni.vdv@gmail.com>
2020-02-01 11:42:38 +0100

    cover the gilded requirements with unit tests
    All tests passes, except one test which covers a new requirement not yet
    implemented.

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -1,12 +1,115 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App;
 
-class GildedRoseTest extends \PHPUnit\Framework\TestCase {
-    public function testFoo() {
-        $items      = [new Item("foo", 0, 0)];
-        $gildedRose = new GildedRose($items);
+use PHPUnit\Framework\TestCase;
+
+final class GildedRoseTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function whenSellDateIsNotPassed_qualityDegrades()
+    {
+        $item = new Item("foo", 1, 10);
+        $gildedRose = new GildedRose([$item]);
         $gildedRose->updateQuality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals(9, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function whenSellDateIsPassed_qualityDegradesTwiceAsFast()
+    {
+        $item = new Item("foo", 0, 10);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertEquals(8, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function whenQualityDropsToZero_ensureItBecomesNotNegative()
+    {
+        $item = new Item("foo", 1, 0);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertEquals(0, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function whenAgedBrieGetsOlder_ensureItsQualityIncreases()
+    {
+        $item = new Item("Aged Brie", 10, 10);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertEquals(11, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function qualityIncreasableItems_shouldNeverBeHigherThan50()
+    {
+        $item = new Item("Aged Brie", 10, 50);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertEquals(50, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function sulfurasItem_shouldNeverBeSoldOrIncreaseInQuality()
+    {
+        $item = new Item("Sulfuras, Hand of Ragnaros", 10, 80);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertEquals(10, $item->sell_in);
+        $this->assertEquals(80, $item->quality);
+    }
+
+    /**
+     * @test
+     * @dataProvider backstagePassesSellInAndQualityExamples
+     * @param int $sellIn
+     * @param int $quality
+     * @param int $expectedRemainingQuality
+     */
+    public function backstagePasses_increasesDifferentlyDependingOnRemainingSellInDays(
+        int $sellIn,
+        int $quality,
+        int $expectedRemainingQuality
+    ) {
+        $item = new Item("Backstage passes to a TAFKAL80ETC concert", $sellIn, $quality);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertEquals($expectedRemainingQuality, $item->quality);
+    }
+
+    public function backstagePassesSellInAndQualityExamples(): array
+    {
+        return [
+            [20, 10, 11],
+            [10, 10, 12],
+            [6, 10, 12],
+            [5, 10, 13],
+            [0, 10, 0],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function conjuredItems_shouldIncreaseTwiceAsFastAsNormalItems()
+    {
+        $item = new Item("Conjured", 10, 50);
+        $gildedRose = new GildedRose([$item]);
+        $gildedRose->updateQuality();
+        $this->assertEquals(48, $item->quality);
     }
 }
